bb8c2b1a5b6dd2db7c404521359af15a3d6ca60b
mohitverma6161 <utkarshmalik06@gmail.com>
2020-02-09 23:00:05 +0530

    redux added

--- App.js
+++ App.js
@@ -6,13 +6,25 @@
  * @flow
  */
 import 'react-native-gesture-handler';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import test from './src/Components/Test';
-
-import React from 'react';
+import DeckList from './src/Components/DeckList';
+import DeckComponent from './src/Components/DeckComponent';
 import MainNavigator from './Navigators/AppNavigator';
+import AddCardComponent from './src/Components/AddCardComponent';
+import QuizComponent from './src/Components/QuizComponent';
+import AddDeck from './src/Components/AddNewDeck';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import Reducers from './src/Reducers/index';
+import ReduxThunk from 'redux-thunk'
+
+
+const store = createStore(Reducers, applyMiddleware(ReduxThunk));
+
 
 import {
   SafeAreaView,
@@ -27,25 +39,59 @@ import {
 
 
 
+
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-export default function App() {
+
+const DeckStackTab = () => {
+
+
   return (
-    <NavigationContainer >
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Dashboard">
-          {
-            () => (
-              <Tab.Navigator >
-                <Tab.Screen name="Decks" component={test} />
-                <Tab.Screen name="AddDeck" component={test} />
-              </Tab.Navigator>
-            )
-          }
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator  >
+      <Stack.Screen name="DeckList" component={DeckList} />
+      <Stack.Screen name="deckmain" component={DeckComponent} />
+      <Stack.Screen name="quiz" component={QuizComponent} />
+      <Stack.Screen name="addCard" component={AddCardComponent} />
+    </Stack.Navigator>
+  )
+
+}
+
+const AddDeckStackTab = () => {
+
+
+  return (
+    <Stack.Navigator  >
+      <Stack.Screen name="addDeck" component={AddDeck} />
+
+    </Stack.Navigator>
+  )
+
+}
+
+
+const MainScreenBottomTab = () => {
+
+  return (
+    <Tab.Navigator >
+      <Tab.Screen name="Decks" component={DeckStackTab} />
+      <Tab.Screen name="AddDeck" component={AddDeckStackTab} />
+    </Tab.Navigator>
+  )
+}
+
+
+export default function App() {
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer >
+        <Stack.Navigator headerMode="none" initialRouteName="Home">
+          <Stack.Screen name="Dashboard" component={MainScreenBottomTab} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
